b0b5c89c446c46551e16ed8cac3ad307193170f8
Niraj Nagtilak <niraj.nagtilak@datatorque.com>
2026-05-22 17:34:01 +1200

    delte unwanted files

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-25613826'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.euq2t3hmpd8"
+    "revision": "0.45d2s5v02j4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
